0a571bb6105339b8b2b15793284606a082d623a4
Vekkumasa <ylirasku@gmail.com>
2022-04-05 19:27:59 +0300

    photonjuttuja

---
+++ Assets/Scripts/ConnectToServer.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using UnityEngine.SceneManagement;
+
+public class ConnectToServer : MonoBehaviourPunCallbacks
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        PhotonNetwork.AutomaticallySyncScene = true;
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+
+}
