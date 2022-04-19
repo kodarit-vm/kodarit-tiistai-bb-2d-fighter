b762602926ae508c5c9957691998c0887f043038
Vekkumasa <ylirasku@gmail.com>
2022-04-19 19:27:43 +0300

    Player spawn

--- Assets/Scripts/SpawnPlayer.cs
+++ Assets/Scripts/SpawnPlayer.cs
@@ -3,12 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
 using Photon.Realtime;
+using System;
 
 public class SpawnPlayer : MonoBehaviour
 {
     public GameObject[] playerPrefabs;
 
-    private float posY = 0f;
+    private float posY = -2f;
     private float player1xPos = -5f;
     private float player2xPos = 5f;
 
@@ -16,7 +17,18 @@ public class SpawnPlayer : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Spawn();
     }
 
+    private void Spawn()
+    {
+        if (PhotonNetwork.LocalPlayer.IsMasterClient)
+        {
+            spawnPos = new Vector2(player1xPos, posY);
+        } else
+        {
+            spawnPos = new Vector2(player2xPos, posY);
+        }
+        PhotonNetwork.Instantiate(playerPrefabs[0].name, spawnPos, Quaternion.identity);
+    }
 }
